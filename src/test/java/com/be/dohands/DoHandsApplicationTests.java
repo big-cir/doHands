074f25d58cc2b82ae13acd8201e8e4deebc20ab2
dae won <eodnjs01477@gmail.com>
2025-01-07 23:51:50 +0900

    init 프로젝트 세팅

---
+++ src/test/java/com/be/dohands/DoHandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.dohands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoHandsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
