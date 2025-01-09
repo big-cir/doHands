1a58d1dd037ab0208e63634d25686f196f1d254c
yikyungbae <yikyung_bae@tmax.co.kr>
2025-01-10 02:25:06 +0900

    [add] 퀘스트 스케쥴 관련 정보 추가 - QuestSchedule entity 추가 - QuestSchedule repository 추가

---
+++ src/main/java/com/be/dohands/quest/repository/QuestScheduleRepository.java
@@ -0,0 +1,8 @@
+package com.be.dohands.quest.repository;
+
+import com.be.dohands.quest.entity.QuestSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestScheduleRepository extends JpaRepository<QuestSchedule, Long> {
+
+}
