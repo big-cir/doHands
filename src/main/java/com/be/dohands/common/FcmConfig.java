335369567a7ac35d836f969c16f8f0c9eda9fd1f
dae won <99483390+big-cir@users.noreply.github.com>
2025-01-09 20:31:13 +0900

    feat: firebase 설정 추가 (#6)

---
+++ src/main/java/com/be/dohands/common/FcmConfig.java
@@ -0,0 +1,42 @@
+package com.be.dohands.common;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class FcmConfig {
+
+    @Value("${fcm.account-path}")
+    private String serviceAccountPath;
+
+    @Value("${fcm.project-id")
+    private String projectId;
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        ClassPathResource serviceAccount = new ClassPathResource(serviceAccountPath);
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+                .setProjectId(projectId)
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            return FirebaseApp.initializeApp(options);
+        }
+
+        return FirebaseApp.getInstance();
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging() throws IOException {
+        return FirebaseMessaging.getInstance(firebaseApp());
+    }
+}
