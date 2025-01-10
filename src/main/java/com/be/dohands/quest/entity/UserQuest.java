da3ed122e02df2f5717152ad378387a94f699bc5
yikyungbae <yikyung_bae@tmax.co.kr>
2025-01-10 22:13:44 +0900

    [add] UserQuest entity 생성

---
+++ src/main/java/com/be/dohands/quest/entity/UserQuest.java
@@ -0,0 +1,36 @@
+package com.be.dohands.quest.entity;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UserQuest {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long userQuestId;
+
+    @Enumerated(value = EnumType.STRING)
+    private QuestType questType;
+
+    private Long questId;
+
+    private Long questExpId;
+
+    private Long userId;
+
+    private Long questScheduleId;
+
+}
