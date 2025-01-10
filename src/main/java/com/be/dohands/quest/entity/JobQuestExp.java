4e7885b167fd7057077327fa440a60c627045088
yikyungbae <yikyung_bae@tmax.co.kr>
2025-01-10 19:28:11 +0900

    [modify] entity의 @Table annotation 삭제

--- src/main/java/com/be/dohands/quest/entity/JobQuestExp.java
+++ src/main/java/com/be/dohands/quest/entity/JobQuestExp.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
