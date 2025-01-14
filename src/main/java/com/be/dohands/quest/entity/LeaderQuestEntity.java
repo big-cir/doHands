a7c6d24a1c636972765f7564edf8bea267d6cadc
juyongc <chunjy2147@gmail.com>
2025-01-14 21:52:42 +0900

    [update] LeaderQuestEntity 수정

--- src/main/java/com/be/dohands/quest/entity/LeaderQuestEntity.java
+++ src/main/java/com/be/dohands/quest/entity/LeaderQuestEntity.java
@@ -5,11 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class LeaderQuestEntity extends BaseTimeEntity {
 
@@ -38,4 +42,6 @@ public class LeaderQuestEntity extends BaseTimeEntity {
     private String notes;
 
     private Integer year;
+
+    private Integer sheetRow;
 }
